90edd2587e30a61cc0b13eedd6327270ee7e351e
Sergey <lavrov-sergey@yandex.ru>
2020-02-17 15:48:55 +0300

    Сделали работающий калькулятор

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,48 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edit1;
+    EditText edit2;
+    RadioGroup radio;
+    TextView textResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        radio = findViewById(R.id.radio);
+        textResult = findViewById(R.id.textResult);
+    }
+
+    public void onClick(View view){
+        int number1 = Integer.parseInt(edit1.getText().toString());
+        int number2 = Integer.parseInt(edit2.getText().toString());
+        int operation = radio.getCheckedRadioButtonId();
+        int result = 0;
+        switch(operation){
+            case R.id.radioPlus:
+                result = number1 + number2;
+                break;
+            case R.id.radioMinus:
+                result = number1 - number2;
+                break;
+            case R.id.radioMulty:
+                result = number1 * number2;
+                break;
+            case R.id.radioDevide:
+                result = number1 / number2;
+                break;
+        }
+        textResult.setText(String.valueOf(result));
     }
 }
