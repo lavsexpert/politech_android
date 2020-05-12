c5a9a25bee0c34124d1df659e4da62dc0e8be8e8
lavsexpert <lavrov-sergey@yandex.ru>
2020-05-12 15:55:40 +0300

    Создан проект по работе с SQL с библиотекой Room

---
+++ 321/Room/app/src/main/java/ru/mospolytech/room/DB.java
@@ -0,0 +1,9 @@
+package ru.mospolytech.room;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {User.class}, version = 1)
+public abstract class DB extends RoomDatabase {
+    public abstract UserDao userDao();
+}
---
+++ 321/Room/app/src/main/java/ru/mospolytech/room/MainActivity.java
@@ -0,0 +1,42 @@
+package ru.mospolytech.room;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editName, editEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editName = findViewById(R.id.editName);
+        editEmail = findViewById(R.id.editEmail);
+    }
+
+    public void onAddClick(View view){
+        User user = new User();
+        user.name = editName.getText().toString();
+        user.email = editEmail.getText().toString();
+        App.db.userDao().insert(user);
+    }
+
+    public void onReadClick(View view){
+        List<User> list = App.db.userDao().readAll();
+        for (User user: list) {
+            Log.d("Polytech", "ID = " + user.id
+            + ", name = " + user.name + ", email = " + user.email);
+        }
+    }
+
+    public void onClearClick(View view){
+        App.db.userDao().clear();
+    }
+}
