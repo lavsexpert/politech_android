8d36d9d7fb7b9cda0b0882b447b30aa7ad7629a6
lavsexpert <lavrov-sergey@yandex.ru>
2020-05-14 09:54:07 +0300

    Создали приложение по взаимодействию с базой данных используя Room

---
+++ 322/Room/app/src/main/java/ru/mospolytech/room/UserDao.java
@@ -0,0 +1,31 @@
+package ru.mospolytech.room;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Insert
+    void create(User user);
+
+    @Query("SELECT * FROM users")
+    List<User> readAll();
+
+    @Query("SELECT * FROM users WHERE name = :name LIMIT 1")
+    User readByName(String name);
+
+    @Update
+    void update(User user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("DELETE FROM users")
+    void clear();
+}
