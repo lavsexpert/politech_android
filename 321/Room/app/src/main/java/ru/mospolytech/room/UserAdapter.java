105babeb9f0a35ce331f07f495415cc6022ccd7e
lavsexpert <lavrov-sergey@yandex.ru>
2020-05-19 15:21:58 +0300

    Добавили выбор и подгрузку элементов списка

--- 321/Room/app/src/main/java/ru/mospolytech/room/UserAdapter.java
+++ 321/Room/app/src/main/java/ru/mospolytech/room/UserAdapter.java
@@ -1,12 +1,16 @@
 package ru.mospolytech.room;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -15,6 +19,8 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     private Context context;
     List<User> list;
+    int selectedPosition;
+    private View selected;
 
     UserAdapter(Context context, List<User> list){
         this.context = context;
@@ -29,11 +35,25 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         User user = list.get(position);
         holder.textID.setText(String.valueOf(user.id));
         holder.textName.setText(user.name);
         holder.textEmail.setText(user.email);
+        holder.line.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                if (selected != null){
+                    selected.setBackgroundColor(0);
+                }
+                v.setBackgroundColor(context.getColor(R.color.colorAccent));
+                selected = v;
+                User user = list.get(position);
+                ((MainActivity) context).editName.setText(user.name);
+                ((MainActivity) context).editEmail.setText(user.email);
+            }
+        });
     }
 
     @Override
@@ -44,12 +64,14 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     class ViewHolder extends RecyclerView.ViewHolder{
 
         TextView textID, textName, textEmail;
+        LinearLayout line;
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
             textID = itemView.findViewById(R.id.textID);
             textName = itemView.findViewById(R.id.textName);
             textEmail = itemView.findViewById(R.id.textEmail);
+            line = itemView.findViewById(R.id.line);
         }
     }
 }
