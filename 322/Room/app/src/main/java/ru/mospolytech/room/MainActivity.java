3c20dcdc5d86127a3b92adba28c03107b3a082c7
lavsexpert <lavrov-sergey@yandex.ru>
2020-05-21 10:08:56 +0300

    Сделали добавление, чтение, удаление и показ списка

--- 322/Room/app/src/main/java/ru/mospolytech/room/MainActivity.java
+++ 322/Room/app/src/main/java/ru/mospolytech/room/MainActivity.java
@@ -1,17 +1,28 @@
 package ru.mospolytech.room;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText editName, editEmail;
+    RecyclerView recyclerView;
+    UserAdapter adapter;
+    List<User> list;
+    static Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,24 +30,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         editName = findViewById(R.id.editName);
         editEmail = findViewById(R.id.editEmail);
+        list = new ArrayList<>();
+        adapter = new UserAdapter(this, list);
+        recyclerView = findViewById(R.id.list);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+        handler = new Handler() {
+            public void handleMessage(@NonNull Message message){
+                adapter.list = list;
+                adapter.notifyDataSetChanged();
+                editName.setText("");
+                editEmail.setText("");
+            }
+        };
     }
 
     public void onAddClick(View view){
-        User user = new User();
-        user.name = editName.getText().toString();
-        user.email = editEmail.getText().toString();
-        App.db.userDao().create(user);
+        Thread dbThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                User user = new User();
+                user.id = App.db.userDao().count();
+                user.name = editName.getText().toString();
+                user.email = editEmail.getText().toString();
+                App.db.userDao().create(user);
+                list.add(user);
+                handler.sendEmptyMessage(0);
+            }
+        });
+        dbThread.start();
     }
 
     public void onReadClick(View view){
-        List<User> list = App.db.userDao().readAll();
-        for (User user: list) {
-            Log.d("Polytech", "ID = "+user.id
-            + ", name = " + user.name + ", email = " + user.email);
-        }
+        Thread dbThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                list.clear();
+                list = App.db.userDao().readAll();
+                for (User user: list) {
+                    Log.d("Polytech", "ID = "+user.id
+                    + ", name = " + user.name + ", email = " + user.email);
+                }
+                handler.sendEmptyMessage(0);
+            }
+        });
+        dbThread.start();
     }
 
     public void onClearClick(View view){
-        App.db.userDao().clear();
+        Thread dbThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                App.db.userDao().clear();
+                list.clear();
+                handler.sendEmptyMessage(0);
+            }
+        });
+        dbThread.start();
     }
 }
